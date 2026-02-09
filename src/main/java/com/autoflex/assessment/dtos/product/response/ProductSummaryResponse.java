908a885bd21da79fdf486b8adccf749e3843d37f
oliveiradniel <kadadniel@gmail.com>
2026-02-09 15:40:43 -0300

    feat: add resource and methods for searching product summaries

---
+++ src/main/java/com/autoflex/assessment/dtos/product/response/ProductSummaryResponse.java
@@ -0,0 +1,10 @@
+package com.autoflex.assessment.dtos.product.response;
+
+public class ProductSummaryResponse {
+
+    public Integer total;
+
+    public Integer active;
+
+    public Integer inactive;
+}
